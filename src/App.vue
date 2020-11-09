9f169b7300633a32983b82ab3eb81b3b7d625b74
Josh Eldridge <jeldridge@michiganlabs.com>
2020-11-08 20:35:05 -0500

    Adding a color palette

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,19 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+@import './styles/colors.scss';
+
+html, body {
+  margin: 0;
+  padding: 0;
+  background-color: $primary-dark;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $secondary-light;
   margin-top: 60px;
 }
 </style>
